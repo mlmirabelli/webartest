85328a8c0554856535ab23eff0611825c8295f13
mlmirabelli <marialaura_mirabelli@gensler.com>
2025-01-22 12:13:32 -0600

    Re-added node_modules for test, converted .nojekyll to _config.yml for test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from '/node_modules/three/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.172.0/build/three.module.js';
 //import * as THREEX from '/node_modules/@ar-js-org/ar.js/three.js/build/ar-threex.mjs';
 //import * as ARjs from 'arjs';
-import { ArToolkitSource, ArToolkitContext, ArMarkerControls }  from '/node_modules/@ar-js-org/ar.js/three.js/build/ar-threex.mjs';
+import { ArToolkitSource, ArToolkitContext, ArMarkerControls }  from 'https://www.npmjs.com/package/ar.js/v/2.1.4/three.js/build/ar-threex.mjs';
 
 ArToolkitContext.baseURL = '../'
 
