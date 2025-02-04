212cfb2398f4545469636d1e24e07b5ac70acf17
mlmirabelli <marialaura_mirabelli@gensler.com>
2025-02-04 09:32:23 -0600

    Reverting y rotation of airplane

--- main.js
+++ main.js
@@ -221,8 +221,8 @@ modelLoader2.load(
 			});
 		object.position.y += 1;
 		object.position.z += 0.5;
-		object.rotation.y -= Math.PI / 2;
-		object.rotation.x -= Math.PI / 4;
+		//object.rotation.y -= Math.PI / 2;
+		object.rotation.x -= Math.PI / 2;
 		object.scale.set(0.00125, 0.00125, 0.00125); 
         markerRoot2.add(object);
 
