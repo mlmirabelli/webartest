f7cf97c7f1ac3828b8a0d7b57e55cc3b086d0143
mlmirabelli <marialaura_mirabelli@gensler.com>
2025-02-04 09:44:25 -0600

    Removing axes helper, retrying y rotation on airplane

--- main.js
+++ main.js
@@ -221,12 +221,10 @@ modelLoader2.load(
 			});
 		object.position.y += 1;
 		object.position.z += 0.5;
-		//object.rotation.y -= Math.PI / 2;
+		object.rotation.y -= Math.PI / 2;
 		//object.rotation.x -= Math.PI / 2;
 		object.scale.set(0.00125, 0.00125, 0.00125); 
         markerRoot2.add(object);
-		const axesHelper = new THREE.AxesHelper( 5 );
-		scene.add( axesHelper );
 
 		var ogVerticalPosition = object.position.z;
 		var movDirection = 1;
