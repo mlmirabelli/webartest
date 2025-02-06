88d9eefe42ee834e0b769085e61addf23e8f6672
mlmirabelli <marialaura_mirabelli@gensler.com>
2025-02-06 10:45:36 -0600

    Testing volcano scale and rotation

--- main.js
+++ main.js
@@ -267,9 +267,9 @@ modelLoader3.load(
 		object.position.y += 1;
 		//object.position.z -= 0.25;
 		object.rotation.y -= Math.PI/ 2;
-		object.rotation.x -= Math.PI / 2;
+		//object.rotation.x -= Math.PI / 2;
 		object.rotation.z -= Math.PI / 2;
-		object.scale.set(0.85, 0.85, 0.85); 
+		object.scale.set(0.5, 0.5, 0.5); 
         markerRoot3.add(object);
     },
     (xhr) => {
