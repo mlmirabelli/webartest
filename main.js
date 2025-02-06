a9774b9b5e47ed565a30108965915a7b489f9ea3
mlmirabelli <marialaura_mirabelli@gensler.com>
2025-02-06 11:17:47 -0600

    Testing volcano and plane rotation

--- main.js
+++ main.js
@@ -266,15 +266,16 @@ modelLoader3.load(
 			});
 		object.position.y += 1;
 		//object.position.z -= 0.25;
-		object.rotation.y -= Math.PI/ 2;
+		//object.rotation.y -= Math.PI/ 2;
 		object.rotation.x -= Math.PI / 2;
-		object.rotation.z += Math.PI / 2;
+		object.rotation.z -= Math.PI / 2;
 		object.scale.set(0.65, 0.65, 0.65); 
         markerRoot3.add(object);
 
 		const planeGeo = new THREE.PlaneGeometry( 1, 1 );
 		const plane = new THREE.Mesh( planeGeo, normalMaterial );
 		plane.rotation.x += Math.PI / 2;
+		plane.position.y += 0.65;
 		markerRoot3.add( plane );
     },
     (xhr) => {
