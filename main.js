e9cc6ac07432ad61eb90141aea2ad2137ab5f099
mlmirabelli <marialaura_mirabelli@gensler.com>
2025-02-10 10:31:22 -0600

    Testing z motion and scaling

--- main.js
+++ main.js
@@ -380,8 +380,8 @@ modelLoader32.load(
 			//object.rotation.y += 0.02
 			console.log("current object.position.x = " + object.position.x);
 			console.log("finalPosition.x = " + finalPosition.x);
-			
-			if(object.position.x < finalPosition.x)
+
+			if(object.position.x + 0.1 < finalPosition.x)
 			{
 				object.position.lerp(finalPosition, 0.0075);
 				//object.scale.set(object.scale.x += scaleFactor, object.scale.y += scaleFactor, object.scale.z += scaleFactor);
